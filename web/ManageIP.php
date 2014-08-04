11459260c981d725a775a77e255d91ab559608a5
toulousain79 <toulousain79@gmail.com>
2014-08-04 11:11:26 +0200

    v1.0

--- web/ManageIP.php
+++ web/ManageIP.php
@@ -127,7 +127,8 @@ if ( isset($_SERVER['PHP_AUTH_USER']) ) {
 			}
 		
 			if ( $new_list == $confirm_list ) {			
-				exec("sudo /bin/bash /etc/MySB/scripts/ManageIpList.sh '".$current_list."' '".$confirm_list."' '".$SeedUser."'", $output, $result);
+				//exec("sudo /bin/bash /etc/MySB/scripts/ManageIpList.sh '".$current_list."' '".$confirm_list."' '".$SeedUser."'", $output, $result);
+				exec("sudo /bin/bash /etc/MySB/scripts/FirewallAndSecurity.sh new '".$SeedUser."' '".$current_list."' '".$confirm_list."'", $output, $result);
 				
 				Form();
 				
