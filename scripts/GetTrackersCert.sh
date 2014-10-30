fff71269708f1772b9151498fe2cb73d1b8c858f
toulousain79 <toulousain79@gmail.com>
2014-10-30 08:05:07 +0100

    v1.2

--- scripts/GetTrackersCert.sh
+++ scripts/GetTrackersCert.sh
@@ -28,7 +28,7 @@ GetCertificate() {
 	TRACKER=$1
 	cd /etc/MySB/ssl/trackers/
 
-	if [ ! -f /etc/ssl/certs/$TRACKER.pem ]; then
+	if [ ! -f /etc/ssl/certs/$TRACKER.pem ] && [ ! -f /etc/MySB/ssl/trackers/$TRACKER.pem ]; then
 		openssl s_client -connect $TRACKER:443 </dev/null 2>/dev/null | sed -n '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' >> ./$TRACKER.crt 
 		if [ -s ./$TRACKER.crt ]; then
 			openssl x509 -in ./$TRACKER.crt -out ./$TRACKER.der -outform DER 
@@ -84,6 +84,8 @@ StatusLSB
 if [ -f /etc/MySB/infos/allow.p2p ]; then
 	mv /etc/MySB/infos/allow.p2p /etc/pgl/allow.p2p
 fi
+
+ScriptInvoke 'source' '/etc/MySB/scripts/FirewallAndSecurity.sh' 'new'
 
 # -----------------------------------------
 source /etc/MySB/inc/includes_after
