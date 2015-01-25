a973c22866b6619eff1f997a74833a7791da5a5f
toulousain79 <toulousain79@gmail.com>
2015-01-25 03:33:50 +0100

    v1.2

--- templates/MySB_2015-01-08.dump.sql
+++ templates/MySB_2015-01-08.dump.sql
@@ -130,26 +130,6 @@ CREATE TABLE trackers_list (
 );
 
 
--- Table: system
-CREATE TABLE system ( 
-    id_system       INTEGER( 1, 1 )  PRIMARY KEY ON CONFLICT IGNORE
-                                     NOT NULL ON CONFLICT ABORT,
-    mysb_version    VARCHAR( 6 )     NOT NULL ON CONFLICT ABORT
-                                     UNIQUE ON CONFLICT IGNORE,
-    mysb_user       VARCHAR( 32 )    UNIQUE ON CONFLICT IGNORE,
-    mysb_password   VARCHAR( 32 )    UNIQUE ON CONFLICT IGNORE,
-    hostname        VARCHAR( 128 )   UNIQUE ON CONFLICT IGNORE,
-    ipv4            VARCHAR( 15 )    UNIQUE ON CONFLICT IGNORE,
-    primary_inet    VARCHAR( 16 )    UNIQUE ON CONFLICT IGNORE,
-    timezone        VARCHAR( 64 )    UNIQUE ON CONFLICT IGNORE,
-    cert_password   VARCHAR( 32 )    UNIQUE ON CONFLICT IGNORE,
-    apt_update      BOOLEAN( 1 )     DEFAULT ( 1 ),
-    apt_date        DATETIME,
-    server_provider VARCHAR( 16 ) 
-);
-
-INSERT INTO [system] ([id_system], [mysb_version], [mysb_user], [mysb_password], [hostname], [ipv4], [primary_inet], [timezone], [cert_password], [apt_update], [apt_date], [server_provider]) VALUES (1, '', '', '', '', '', '', '', '', 0, '', '');
-
 -- Table: commands
 CREATE TABLE commands ( 
     id_commands INTEGER         PRIMARY KEY ON CONFLICT IGNORE AUTOINCREMENT
@@ -162,12 +142,6 @@ CREATE TABLE commands (
     user        VARCHAR( 16 )   NOT NULL ON CONFLICT ABORT 
 );
 
-INSERT INTO [commands] ([id_commands], [commands], [reload], [priority], [args], [user]) VALUES (1, 'MySB_SecurityRules', 1, 1, '', 'elohim13');
-INSERT INTO [commands] ([id_commands], [commands], [reload], [priority], [args], [user]) VALUES (2, 'MySB_SecurityRules', 1, 2, '', 'elohim13');
-INSERT INTO [commands] ([id_commands], [commands], [reload], [priority], [args], [user]) VALUES (3, 'MySB_SecurityRules', 1, 3, '', 'elohim13');
-INSERT INTO [commands] ([id_commands], [commands], [reload], [priority], [args], [user]) VALUES (4, 'MySB_SecurityRules', 1, 4, '', 'elohim13');
-INSERT INTO [commands] ([id_commands], [commands], [reload], [priority], [args], [user]) VALUES (5, 'Postfix', 1, 5, '', 'elohim13');
-INSERT INTO [commands] ([id_commands], [commands], [reload], [priority], [args], [user]) VALUES (6, 'MySB_SecurityRules', 1, 6, null, 'elohim13');
 
 -- Table: dnscrypt_resolvers
 CREATE TABLE dnscrypt_resolvers ( 
@@ -344,3 +318,24 @@ INSERT INTO [providers_monitoring] ([id_providers_monitoring], [provider], [ipv4
 INSERT INTO [providers_monitoring] ([id_providers_monitoring], [provider], [ipv4], [hostname]) VALUES (4, 'OVH', null, 'proxy.sbg.ovh.net');
 INSERT INTO [providers_monitoring] ([id_providers_monitoring], [provider], [ipv4], [hostname]) VALUES (5, 'OVH', null, 'proxy.bhs.ovh.net');
 INSERT INTO [providers_monitoring] ([id_providers_monitoring], [provider], [ipv4], [hostname]) VALUES (6, 'OVH', null, 'ping.ovh.net');
+
+-- Table: system
+CREATE TABLE system ( 
+    id_system       INTEGER( 1, 1 )  PRIMARY KEY ON CONFLICT IGNORE
+                                     NOT NULL ON CONFLICT ABORT,
+    mysb_version    VARCHAR( 6 )     NOT NULL ON CONFLICT ABORT
+                                     UNIQUE ON CONFLICT IGNORE,
+    mysb_user       VARCHAR( 32 )    UNIQUE ON CONFLICT IGNORE,
+    mysb_password   VARCHAR( 32 )    UNIQUE ON CONFLICT IGNORE,
+    hostname        VARCHAR( 128 )   UNIQUE ON CONFLICT IGNORE,
+    ipv4            VARCHAR( 15 )    UNIQUE ON CONFLICT IGNORE,
+    primary_inet    VARCHAR( 16 )    UNIQUE ON CONFLICT IGNORE,
+    timezone        VARCHAR( 64 )    UNIQUE ON CONFLICT IGNORE,
+    cert_password   VARCHAR( 32 )    UNIQUE ON CONFLICT IGNORE,
+    apt_update      BOOLEAN( 1 )     DEFAULT ( 1 ),
+    apt_date        DATETIME,
+    server_provider VARCHAR( 16 ),
+    ip_restriction  BOOLEAN( 1 )     DEFAULT ( 1 ) 
+);
+
+INSERT INTO [system] ([id_system], [mysb_version], [mysb_user], [mysb_password], [hostname], [ipv4], [primary_inet], [timezone], [cert_password], [apt_update], [apt_date], [server_provider], [ip_restriction]) VALUES (1, '', '', '', '', '', '', '', '', 0, '', '', 1);
