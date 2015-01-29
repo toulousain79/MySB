01747447e6d20bef2c95dad93fb576d92295d047
toulousain79 <toulousain79@gmail.com>
2015-01-29 09:06:28 +0100

    v1.2

--- web/pages/Layout.php
+++ web/pages/Layout.php
@@ -135,8 +135,8 @@
 			endforeach;
 			echo $FooterNavBar . '<br /><br />';
 ?>
-			<a href="https://github.com/toulousain79/MySB/" title="MySB on GitHub">MySB on GitHub</a><br />
-			<a href="http://www.css3templates.co.uk">Copyright &copy; CSS3_two</a> | <a href="http://www.wolfcms.org/" title="Wolf CMS">Wolf CMS</a>
+			<a target="_blank" href="https://github.com/toulousain79/MySB/" title="MySB on GitHub">MySB on GitHub</a> | <a target="_blank" href="https://github.com/toulousain79/MySB/blob/<?php echo GetVersion(); ?>/Changelog.md" title="MySB on GitHub">Changelog <?php echo GetVersion(); ?></a><br />
+			<a target="_blank" href="http://www.css3templates.co.uk">Copyright &copy; CSS3_two</a> | <a target="_blank" href="http://www.wolfcms.org/" title="Wolf CMS">Wolf CMS</a>
 		</footer>
 	</div>
 
