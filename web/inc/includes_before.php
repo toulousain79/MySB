b3aa90449d4507dcef2f10aa416a39c8c18e4b0c
toulousain79 <toulousain79@users.noreply.github.com>
2015-05-17 01:20:31 +0200

    v2.2

--- web/inc/includes_before.php
+++ web/inc/includes_before.php
@@ -22,6 +22,7 @@
 //
 //#################### FIRST LINE #####################################
 
+// Config file
 require_once('/etc/MySB/config.php');
 
 // Session
@@ -41,6 +42,34 @@ if ( isset($_GET['page']) ) {
 			break;
 	}
 }
+
+header('Cache-control: private'); // IE 6 FIX
+if ( isSet($_GET['lang']) ) {
+	$lang = $_GET['lang'];
+
+	// register the session and set the cookie
+	$_SESSION['lang'] = $lang;
+
+	setcookie('lang', $lang, time() + (3600 * 24 * 30));
+} else if ( isSet($_SESSION['lang']) ) {
+	$lang = $_SESSION['lang'];
+} else if ( isSet($_COOKIE['lang']) ) {
+	$lang = $_COOKIE['lang'];
+} else {
+	$lang = 'en';
+}
+
+switch ($lang) {
+	case 'fr':
+	$lang_file = 'lang.en.php';
+	break;
+
+	default:
+	$lang_file = 'lang.en.php';
+}
+
+// Language
+require_once(WEB_INC . '/' . $lang_file);
 
 // Medoo framework
 require_once(FILE_MEDOO);
