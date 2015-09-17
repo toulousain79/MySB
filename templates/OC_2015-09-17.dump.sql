1c05859e7a7ad4dc6d69ff467ffd66056e82992f
toulousain79 <toulousain79@users.noreply.github.com>
2015-09-17 23:32:16 +0200

    v2.2

---
+++ templates/OC_2015-09-17.dump.sql
@@ -0,0 +1,412 @@
+-- MySQL dump 10.13  Distrib 5.6.25, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: owncloud_db
+-- ------------------------------------------------------
+-- Server version	5.6.25-1~dotdeb+7.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `oc_activity`
+--
+
+DROP TABLE IF EXISTS `oc_activity`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_activity` (
+  `activity_id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` int(11) NOT NULL DEFAULT '0',
+  `priority` int(11) NOT NULL DEFAULT '0',
+  `type` varchar(255) COLLATE utf8_bin NOT NULL,
+  `user` varchar(64) COLLATE utf8_bin NOT NULL,
+  `affecteduser` varchar(64) COLLATE utf8_bin NOT NULL,
+  `app` varchar(255) COLLATE utf8_bin NOT NULL,
+  `subject` varchar(255) COLLATE utf8_bin NOT NULL,
+  `subjectparams` varchar(4000) COLLATE utf8_bin NOT NULL,
+  `message` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `messageparams` varchar(4000) COLLATE utf8_bin DEFAULT NULL,
+  `file` varchar(4000) COLLATE utf8_bin DEFAULT NULL,
+  `link` varchar(4000) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`activity_id`),
+  KEY `activity_user_time` (`affecteduser`,`timestamp`),
+  KEY `activity_filter_by` (`affecteduser`,`user`,`timestamp`),
+  KEY `activity_filter_app` (`affecteduser`,`app`,`timestamp`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_activity_mq`
+--
+
+DROP TABLE IF EXISTS `oc_activity_mq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_activity_mq` (
+  `mail_id` int(11) NOT NULL AUTO_INCREMENT,
+  `amq_timestamp` int(11) NOT NULL DEFAULT '0',
+  `amq_latest_send` int(11) NOT NULL DEFAULT '0',
+  `amq_type` varchar(255) COLLATE utf8_bin NOT NULL,
+  `amq_affecteduser` varchar(64) COLLATE utf8_bin NOT NULL,
+  `amq_appid` varchar(255) COLLATE utf8_bin NOT NULL,
+  `amq_subject` varchar(255) COLLATE utf8_bin NOT NULL,
+  `amq_subjectparams` varchar(4000) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`mail_id`),
+  KEY `amp_user` (`amq_affecteduser`),
+  KEY `amp_latest_send_time` (`amq_latest_send`),
+  KEY `amp_timestamp_time` (`amq_timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_appconfig`
+--
+
+DROP TABLE IF EXISTS `oc_appconfig`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_appconfig` (
+  `appid` varchar(32) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `configkey` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `configvalue` longtext COLLATE utf8_bin,
+  PRIMARY KEY (`appid`,`configkey`),
+  KEY `appconfig_config_key_index` (`configkey`),
+  KEY `appconfig_appid_key` (`appid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_file_map`
+--
+
+DROP TABLE IF EXISTS `oc_file_map`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_file_map` (
+  `logic_path` varchar(512) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `logic_path_hash` varchar(32) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `physic_path` varchar(512) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `physic_path_hash` varchar(32) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`logic_path_hash`),
+  UNIQUE KEY `file_map_pp_index` (`physic_path_hash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_filecache`
+--
+
+DROP TABLE IF EXISTS `oc_filecache`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_filecache` (
+  `fileid` int(11) NOT NULL AUTO_INCREMENT,
+  `storage` int(11) NOT NULL DEFAULT '0',
+  `path` varchar(4000) COLLATE utf8_bin DEFAULT NULL,
+  `path_hash` varchar(32) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `parent` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `mimetype` int(11) NOT NULL DEFAULT '0',
+  `mimepart` int(11) NOT NULL DEFAULT '0',
+  `size` bigint(20) NOT NULL DEFAULT '0',
+  `mtime` int(11) NOT NULL DEFAULT '0',
+  `storage_mtime` int(11) NOT NULL DEFAULT '0',
+  `encrypted` int(11) NOT NULL DEFAULT '0',
+  `unencrypted_size` bigint(20) NOT NULL DEFAULT '0',
+  `etag` varchar(40) COLLATE utf8_bin DEFAULT NULL,
+  `permissions` int(11) DEFAULT '0',
+  PRIMARY KEY (`fileid`),
+  UNIQUE KEY `fs_storage_path_hash` (`storage`,`path_hash`),
+  KEY `fs_parent_name_hash` (`parent`,`name`),
+  KEY `fs_storage_mimetype` (`storage`,`mimetype`),
+  KEY `fs_storage_mimepart` (`storage`,`mimepart`),
+  KEY `fs_storage_size` (`storage`,`size`,`fileid`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_files_trash`
+--
+
+DROP TABLE IF EXISTS `oc_files_trash`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_files_trash` (
+  `auto_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(250) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `timestamp` varchar(12) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `location` varchar(512) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `type` varchar(4) COLLATE utf8_bin DEFAULT NULL,
+  `mime` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`auto_id`),
+  KEY `id_index` (`id`),
+  KEY `timestamp_index` (`timestamp`),
+  KEY `user_index` (`user`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_group_admin`
+--
+
+DROP TABLE IF EXISTS `oc_group_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_group_admin` (
+  `gid` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `uid` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`gid`,`uid`),
+  KEY `group_admin_uid` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_group_user`
+--
+
+DROP TABLE IF EXISTS `oc_group_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_group_user` (
+  `gid` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `uid` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`gid`,`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_groups`
+--
+
+DROP TABLE IF EXISTS `oc_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_groups` (
+  `gid` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`gid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_jobs`
+--
+
+DROP TABLE IF EXISTS `oc_jobs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_jobs` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `class` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `argument` varchar(4000) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `last_run` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `job_class_index` (`class`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_mimetypes`
+--
+
+DROP TABLE IF EXISTS `oc_mimetypes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_mimetypes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mimetype` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `mimetype_id_index` (`mimetype`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_preferences`
+--
+
+DROP TABLE IF EXISTS `oc_preferences`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_preferences` (
+  `userid` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `appid` varchar(32) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `configkey` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `configvalue` longtext COLLATE utf8_bin,
+  PRIMARY KEY (`userid`,`appid`,`configkey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_privatedata`
+--
+
+DROP TABLE IF EXISTS `oc_privatedata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_privatedata` (
+  `keyid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `app` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `key` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `value` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`keyid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_properties`
+--
+
+DROP TABLE IF EXISTS `oc_properties`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_properties` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `propertypath` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `propertyname` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `propertyvalue` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `property_index` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_share`
+--
+
+DROP TABLE IF EXISTS `oc_share`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_share` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `share_type` smallint(6) NOT NULL DEFAULT '0',
+  `share_with` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `uid_owner` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `parent` int(11) DEFAULT NULL,
+  `item_type` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `item_source` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `item_target` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `file_source` int(11) DEFAULT NULL,
+  `file_target` varchar(512) COLLATE utf8_bin DEFAULT NULL,
+  `permissions` smallint(6) NOT NULL DEFAULT '0',
+  `stime` bigint(20) NOT NULL DEFAULT '0',
+  `accepted` smallint(6) NOT NULL DEFAULT '0',
+  `expiration` datetime DEFAULT NULL,
+  `token` varchar(32) COLLATE utf8_bin DEFAULT NULL,
+  `mail_send` smallint(6) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `item_share_type_index` (`item_type`,`share_type`),
+  KEY `file_source_index` (`file_source`),
+  KEY `token_index` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_share_external`
+--
+
+DROP TABLE IF EXISTS `oc_share_external`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_share_external` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `remote` varchar(512) COLLATE utf8_bin NOT NULL COMMENT 'Url of the remove owncloud instance',
+  `remote_id` int(11) NOT NULL DEFAULT '-1',
+  `share_token` varchar(64) COLLATE utf8_bin NOT NULL COMMENT 'Public share token',
+  `password` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT 'Optional password for the public share',
+  `name` varchar(64) COLLATE utf8_bin NOT NULL COMMENT 'Original name on the remote server',
+  `owner` varchar(64) COLLATE utf8_bin NOT NULL COMMENT 'User that owns the public share on the remote server',
+  `user` varchar(64) COLLATE utf8_bin NOT NULL COMMENT 'Local user which added the external share',
+  `mountpoint` varchar(4000) COLLATE utf8_bin NOT NULL COMMENT 'Full path where the share is mounted',
+  `mountpoint_hash` varchar(32) COLLATE utf8_bin NOT NULL COMMENT 'md5 hash of the mountpoint',
+  `accepted` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `sh_external_mp` (`user`,`mountpoint_hash`),
+  KEY `sh_external_user` (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_storages`
+--
+
+DROP TABLE IF EXISTS `oc_storages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_storages` (
+  `id` varchar(64) COLLATE utf8_bin DEFAULT NULL,
+  `numeric_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`numeric_id`),
+  UNIQUE KEY `storages_id_index` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_users`
+--
+
+DROP TABLE IF EXISTS `oc_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_users` (
+  `uid` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `displayname` varchar(64) COLLATE utf8_bin DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_vcategory`
+--
+
+DROP TABLE IF EXISTS `oc_vcategory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_vcategory` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `type` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `category` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `uid_index` (`uid`),
+  KEY `type_index` (`type`),
+  KEY `category_index` (`category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `oc_vcategory_to_object`
+--
+
+DROP TABLE IF EXISTS `oc_vcategory_to_object`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `oc_vcategory_to_object` (
+  `objid` int(10) unsigned NOT NULL DEFAULT '0',
+  `categoryid` int(10) unsigned NOT NULL DEFAULT '0',
+  `type` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`categoryid`,`objid`,`type`),
+  KEY `vcategory_objectd_index` (`objid`,`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-09-17 23:24:45
