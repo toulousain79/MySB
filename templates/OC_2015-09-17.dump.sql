dbb8e4cca6e63dfd2c1953abeb602eef3ae06866
toulousain79 <toulousain79@users.noreply.github.com>
2015-09-20 20:12:14 +0200

    v2.2

--- templates/OC_2015-09-17.dump.sql
+++ templates/OC_2015-09-17.dump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.6.25, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: owncloud_db
+-- Host: localhost    Database: ownCloud_db
 -- ------------------------------------------------------
 -- Server version	5.6.25-1~dotdeb+7.1
 
@@ -26,8 +26,8 @@ CREATE TABLE `oc_activity` (
   `activity_id` int(11) NOT NULL AUTO_INCREMENT,
   `timestamp` int(11) NOT NULL DEFAULT '0',
   `priority` int(11) NOT NULL DEFAULT '0',
-  `type` varchar(255) COLLATE utf8_bin NOT NULL,
-  `user` varchar(64) COLLATE utf8_bin NOT NULL,
+  `type` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `user` varchar(64) COLLATE utf8_bin DEFAULT NULL,
   `affecteduser` varchar(64) COLLATE utf8_bin NOT NULL,
   `app` varchar(255) COLLATE utf8_bin NOT NULL,
   `subject` varchar(255) COLLATE utf8_bin NOT NULL,
@@ -42,6 +42,15 @@ CREATE TABLE `oc_activity` (
   KEY `activity_filter_app` (`affecteduser`,`app`,`timestamp`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_activity`
+--
+
+LOCK TABLES `oc_activity` WRITE;
+/*!40000 ALTER TABLE `oc_activity` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_activity` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `oc_activity_mq`
@@ -67,6 +76,15 @@ CREATE TABLE `oc_activity_mq` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `oc_activity_mq`
+--
+
+LOCK TABLES `oc_activity_mq` WRITE;
+/*!40000 ALTER TABLE `oc_activity_mq` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_activity_mq` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `oc_appconfig`
 --
 
@@ -84,6 +102,16 @@ CREATE TABLE `oc_appconfig` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `oc_appconfig`
+--
+
+LOCK TABLES `oc_appconfig` WRITE;
+/*!40000 ALTER TABLE `oc_appconfig` DISABLE KEYS */;
+INSERT INTO `oc_appconfig` VALUES ('activity','enabled','yes'),('activity','installed_version','2.0.2'),('activity','types','filesystem'),('backgroundjob','lastjob','4'),('core','OC_Channel','production'),('core','backgroundjobs_mode','cron'),('core','installedat','1442772094.5885'),('core','lastcron','1442772185'),('core','lastupdateResult','{\"version\":{},\"versionstring\":{},\"url\":{},\"web\":{}}'),('core','lastupdatedat','1442772219'),('core','public_files','files_sharing/public.php'),('core','public_gallery','gallery/public.php'),('core','public_webdav','files_sharing/publicwebdav.php'),('core','remote_files','files/appinfo/remote.php'),('core','remote_webdav','files/appinfo/remote.php'),('files','enabled','yes'),('files','installed_version','1.1.10'),('files','types','filesystem'),('files_locking','enabled','yes'),('files_locking','installed_version',''),('files_locking','types','filesystem'),('files_pdfviewer','enabled','yes'),('files_pdfviewer','installed_version','0.7'),('files_pdfviewer','ocsid','166049'),('files_pdfviewer','types',''),('files_sharing','enabled','yes'),('files_sharing','installed_version','0.6.2'),('files_sharing','types','filesystem'),('files_texteditor','enabled','yes'),('files_texteditor','installed_version','0.4'),('files_texteditor','ocsid','166051'),('files_texteditor','types',''),('files_trashbin','enabled','yes'),('files_trashbin','installed_version','0.6.3'),('files_trashbin','types','filesystem'),('files_versions','enabled','yes'),('files_versions','installed_version','1.0.6'),('files_versions','types','filesystem'),('files_videoviewer','enabled','yes'),('files_videoviewer','installed_version','0.1.3'),('files_videoviewer','ocsid','166054'),('files_videoviewer','types',''),('firstrunwizard','enabled','yes'),('firstrunwizard','installed_version','1.1'),('firstrunwizard','ocsid','166055'),('firstrunwizard','types',''),('gallery','enabled','yes'),('gallery','installed_version','0.6.0'),('gallery','types',''),('provisioning_api','enabled','yes'),('provisioning_api','installed_version','0.2'),('provisioning_api','types','filesystem'),('templateeditor','enabled','yes'),('templateeditor','installed_version','0.1'),('templateeditor','types',''),('updater','enabled','yes'),('updater','installed_version','0.6'),('updater','types','');
+/*!40000 ALTER TABLE `oc_appconfig` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `oc_file_map`
 --
 
@@ -99,6 +127,15 @@ CREATE TABLE `oc_file_map` (
   UNIQUE KEY `file_map_pp_index` (`physic_path_hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_file_map`
+--
+
+LOCK TABLES `oc_file_map` WRITE;
+/*!40000 ALTER TABLE `oc_file_map` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_file_map` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `oc_filecache`
@@ -129,8 +166,17 @@ CREATE TABLE `oc_filecache` (
   KEY `fs_storage_mimetype` (`storage`,`mimetype`),
   KEY `fs_storage_mimepart` (`storage`,`mimepart`),
   KEY `fs_storage_size` (`storage`,`size`,`fileid`)
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_filecache`
+--
+
+LOCK TABLES `oc_filecache` WRITE;
+/*!40000 ALTER TABLE `oc_filecache` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_filecache` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `oc_files_trash`
@@ -155,6 +201,15 @@ CREATE TABLE `oc_files_trash` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `oc_files_trash`
+--
+
+LOCK TABLES `oc_files_trash` WRITE;
+/*!40000 ALTER TABLE `oc_files_trash` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_files_trash` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `oc_group_admin`
 --
 
@@ -168,6 +223,15 @@ CREATE TABLE `oc_group_admin` (
   KEY `group_admin_uid` (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_group_admin`
+--
+
+LOCK TABLES `oc_group_admin` WRITE;
+/*!40000 ALTER TABLE `oc_group_admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_group_admin` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `oc_group_user`
@@ -184,6 +248,16 @@ CREATE TABLE `oc_group_user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `oc_group_user`
+--
+
+LOCK TABLES `oc_group_user` WRITE;
+/*!40000 ALTER TABLE `oc_group_user` DISABLE KEYS */;
+INSERT INTO `oc_group_user` VALUES ('admin','admin');
+/*!40000 ALTER TABLE `oc_group_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `oc_groups`
 --
 
@@ -195,6 +269,16 @@ CREATE TABLE `oc_groups` (
   PRIMARY KEY (`gid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_groups`
+--
+
+LOCK TABLES `oc_groups` WRITE;
+/*!40000 ALTER TABLE `oc_groups` DISABLE KEYS */;
+INSERT INTO `oc_groups` VALUES ('MySB'),('admin');
+/*!40000 ALTER TABLE `oc_groups` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `oc_jobs`
@@ -214,6 +298,16 @@ CREATE TABLE `oc_jobs` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `oc_jobs`
+--
+
+LOCK TABLES `oc_jobs` WRITE;
+/*!40000 ALTER TABLE `oc_jobs` DISABLE KEYS */;
+INSERT INTO `oc_jobs` VALUES (1,'OCA\\Activity\\BackgroundJob\\EmailNotification','null',1442772096),(2,'OCA\\Activity\\BackgroundJob\\ExpireActivities','null',1442772116),(3,'OCA\\Files_sharing\\Lib\\DeleteOrphanedSharesJob','null',1442772182),(5,'OC\\Command\\CommandJob','\"O:33:\\\"OCA\\\\Files_Trashbin\\\\Command\\\\Expire\\\":2:{s:39:\\\"\\u0000OCA\\\\Files_Trashbin\\\\Command\\\\Expire\\u0000user\\\";s:5:\\\"admin\\\";s:47:\\\"\\u0000OCA\\\\Files_Trashbin\\\\Command\\\\Expire\\u0000trashBinSize\\\";i:36227;}\"',0),(6,'OC\\Command\\CommandJob','\"O:33:\\\"OCA\\\\Files_Trashbin\\\\Command\\\\Expire\\\":2:{s:39:\\\"\\u0000OCA\\\\Files_Trashbin\\\\Command\\\\Expire\\u0000user\\\";s:5:\\\"admin\\\";s:47:\\\"\\u0000OCA\\\\Files_Trashbin\\\\Command\\\\Expire\\u0000trashBinSize\\\";i:2956667;}\"',0);
+/*!40000 ALTER TABLE `oc_jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `oc_mimetypes`
 --
 
@@ -225,8 +319,18 @@ CREATE TABLE `oc_mimetypes` (
   `mimetype` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `mimetype_id_index` (`mimetype`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_mimetypes`
+--
+
+LOCK TABLES `oc_mimetypes` WRITE;
+/*!40000 ALTER TABLE `oc_mimetypes` DISABLE KEYS */;
+INSERT INTO `oc_mimetypes` VALUES (3,'application'),(8,'application/octet-stream'),(7,'application/pdf'),(4,'application/vnd.oasis.opendocument.text'),(1,'httpd'),(2,'httpd/unix-directory'),(5,'image'),(6,'image/jpeg');
+/*!40000 ALTER TABLE `oc_mimetypes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `oc_preferences`
@@ -243,6 +347,16 @@ CREATE TABLE `oc_preferences` (
   PRIMARY KEY (`userid`,`appid`,`configkey`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_preferences`
+--
+
+LOCK TABLES `oc_preferences` WRITE;
+/*!40000 ALTER TABLE `oc_preferences` DISABLE KEYS */;
+INSERT INTO `oc_preferences` VALUES ('admin','firstrunwizard','show','0'),('admin','login','lastLogin','1442772094');
+/*!40000 ALTER TABLE `oc_preferences` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `oc_privatedata`
@@ -262,6 +376,15 @@ CREATE TABLE `oc_privatedata` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `oc_privatedata`
+--
+
+LOCK TABLES `oc_privatedata` WRITE;
+/*!40000 ALTER TABLE `oc_privatedata` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_privatedata` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `oc_properties`
 --
 
@@ -278,6 +401,15 @@ CREATE TABLE `oc_properties` (
   KEY `property_index` (`userid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_properties`
+--
+
+LOCK TABLES `oc_properties` WRITE;
+/*!40000 ALTER TABLE `oc_properties` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_properties` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `oc_share`
@@ -311,6 +443,15 @@ CREATE TABLE `oc_share` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `oc_share`
+--
+
+LOCK TABLES `oc_share` WRITE;
+/*!40000 ALTER TABLE `oc_share` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_share` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `oc_share_external`
 --
 
@@ -336,6 +477,15 @@ CREATE TABLE `oc_share_external` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `oc_share_external`
+--
+
+LOCK TABLES `oc_share_external` WRITE;
+/*!40000 ALTER TABLE `oc_share_external` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_share_external` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `oc_storages`
 --
 
@@ -347,8 +497,18 @@ CREATE TABLE `oc_storages` (
   `numeric_id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`numeric_id`),
   UNIQUE KEY `storages_id_index` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_storages`
+--
+
+LOCK TABLES `oc_storages` WRITE;
+/*!40000 ALTER TABLE `oc_storages` DISABLE KEYS */;
+INSERT INTO `oc_storages` VALUES ('home::admin',1),('local::/home/owncloud/',2);
+/*!40000 ALTER TABLE `oc_storages` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `oc_users`
@@ -364,6 +524,16 @@ CREATE TABLE `oc_users` (
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_users`
+--
+
+LOCK TABLES `oc_users` WRITE;
+/*!40000 ALTER TABLE `oc_users` DISABLE KEYS */;
+INSERT INTO `oc_users` VALUES ('admin',NULL,'1|$2y$10$iPeDQQ2/k1rE4UkjwDwioOqGeP44EXR3CCDh21CNlSYuUnqQ9u.Ta');
+/*!40000 ALTER TABLE `oc_users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `oc_vcategory`
@@ -385,6 +555,15 @@ CREATE TABLE `oc_vcategory` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `oc_vcategory`
+--
+
+LOCK TABLES `oc_vcategory` WRITE;
+/*!40000 ALTER TABLE `oc_vcategory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_vcategory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `oc_vcategory_to_object`
 --
 
@@ -399,6 +578,23 @@ CREATE TABLE `oc_vcategory_to_object` (
   KEY `vcategory_objectd_index` (`objid`,`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oc_vcategory_to_object`
+--
+
+LOCK TABLES `oc_vcategory_to_object` WRITE;
+/*!40000 ALTER TABLE `oc_vcategory_to_object` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oc_vcategory_to_object` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'ownCloud_db'
+--
+
+--
+-- Dumping routines for database 'ownCloud_db'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -409,4 +605,4 @@ CREATE TABLE `oc_vcategory_to_object` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-09-17 23:24:45
+-- Dump completed on 2015-09-20 20:05:57
