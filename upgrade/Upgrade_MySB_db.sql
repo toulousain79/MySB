0fed22bc35180cf65600ddd2b5757471afc7e02d
toulousain79 <toulousain79@users.noreply.github.com>
2015-11-12 13:34:04 +0100

    v3.1

--- upgrade/Upgrade_MySB_db.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `providers_monitoring` (`provider`, `ipv4`, `hostname`) VALUES ('DIGICUBE', '95.130.8.5/32', 'digi00003.digicube.fr');
-INSERT INTO `providers_monitoring` (`provider`, `ipv4`, `hostname`) VALUES ('DIGICUBE', '95.130.8.210/32', 'www.digicube.fr');
