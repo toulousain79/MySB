e5aaf43ba9d72d0da42a2f66693290b0c52a5e4d
toulousain79 <toulousain79@users.noreply.github.com>
2016-01-25 13:34:32 +0100

    v3.2

--- templates/Dump_MySQL_MySB_db_Schema_2016-01-12.sql
+++ templates/Dump_MySQL_MySB_db_Schema_2016-01-12.sql
@@ -138,6 +138,7 @@ CREATE TABLE IF NOT EXISTS `system` (
   `pgl_email_stats` tinyint(1) NOT NULL DEFAULT '0',
   `pgl_watchdog_email` tinyint(1) NOT NULL DEFAULT '0',
   `dnscrypt` tinyint(1) NOT NULL DEFAULT '0',
+  `owncloud_cron` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_system`),
   UNIQUE KEY `mysb_version` (`mysb_version`,`mysb_user`,`mysb_password`,`hostname`,`ipv4`,`primary_inet`,`timezone`,`cert_password`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
