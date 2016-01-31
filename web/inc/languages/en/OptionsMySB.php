d28c18df27cc4212c35c30762e5947d64ed589ad
toulousain79 <toulousain79@users.noreply.github.com>
2016-01-31 10:36:58 +0100

    v3.2

--- web/inc/languages/en/OptionsMySB.php
+++ web/inc/languages/en/OptionsMySB.php
@@ -48,8 +48,8 @@ define('User_OptionsMySB_NumDay', 'Week<br/>days');
 define('User_OptionsMySB_Command', 'Script');
 define('User_OptionsMySB_Add', 'Add');
 define('User_OptionsMySB_Crontab_Comment', 'Schedule your personal scripts at the end of your downloads.<br/>
-Your scripts must be stored in ~/crontab/ with extension ".cron".
-Confirm your schedule here ;-) <a target="_blank" href="http://crontab.guru/">crontab.guru</a>.
+Your scripts must be stored in ~/crontab/ with extension ".cron".<br/>
+Confirm your schedule here ;-) <a target="_blank" href="http://crontab.guru/">crontab.guru</a>.<br/>
 Read the file \'~/scripts/README\'.');
 
 //#################### LAST LINE ######################################
