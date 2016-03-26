4955c7c435fb577b9c8d71b9849f7693f0378f8b
toulousain79 <toulousain79@users.noreply.github.com>
2016-03-26 09:50:45 +0100

    v3.3

--- templates/Dump_MySQL_MySB_db_Data_2016-02-03.sql
+++ templates/Dump_MySQL_MySB_db_Data_2016-02-03.sql
@@ -59,7 +59,16 @@ INSERT INTO `providers_monitoring` (`id_providers_monitoring`, `provider`, `ipv4
 (5, 'OVH', '', 'proxy-rbx.ovh.net'),
 (6, 'OVH', '', 'proxy.sbg.ovh.net'),
 (7, 'OVH', '', 'proxy.bhs.ovh.net'),
-(8, 'OVH', '', 'ping.ovh.net');
+(8, 'OVH', '', 'ping.ovh.net'),
+(9, 'OVH', '', 'proxy.p19.ovh.net'),
+(10, 'OVH', '', 'proxy.ovh.net'),
+(11, 'OVH', '92.222.184.0/24', ''),
+(12, 'OVH', '92.222.185.0/24', ''),
+(13, 'OVH', '92.222.186.0/24', ''),
+(14, 'OVH', '167.114.37.0/24', ''),
+(15, 'OVH', '151.80.231.244/32', ''),
+(16, 'OVH', '151.80.231.245/32', ''),
+(17, 'OVH', '151.80.231.246/32', '');
 
 INSERT INTO `renting` (`id_renting`, `model`, `tva`, `global_cost`, `nb_users`, `price_per_users`, `method`) VALUES
 (1, '', 0.00, 0.00, 0, 0.00, 0);
