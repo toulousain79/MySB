123f6d085ff0b84bcc95d818037d506c56a37294
toulousain79 <toulousain79@users.noreply.github.com>
2016-12-29 00:12:27 +0100

    v3.5

---
+++ upgrade/lets_encrypt.sql
@@ -0,0 +1,11 @@
+INSERT INTO `lets_encrypt` (`id_lets_encrypt`, `addresses`, `ipv4`) VALUES
+(1, 'acme-v01.api.letsencrypt.org', '104.98.130.119'),
+(2, 'acme-staging.api.letsencrypt.org', '92.122.117.72'),
+(3, 'ocsp.staging-x1.letsencrypt.org', '2.16.4.219'),
+(4, 'ocsp.root-x1.letsencrypt.org', '195.215.247.8'),
+(5, 'ocsp.int-x1.letsencrypt.org', '88.221.144.27'),
+(6, 'ocsp.int-x2.letsencrypt.org', '23.65.29.67'),
+(7, 'ocsp.int-x3.letsencrypt.org', '81.25.198.161'),
+(8, 'ocsp.int-x4.letsencrypt.org', '81.25.198.248'),
+(9, 'outbound1.letsencrypt.org', '66.133.109.36'),
+(10, 'outbound2.letsencrypt.org', '64.78.149.164');
