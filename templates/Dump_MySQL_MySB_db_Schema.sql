3b885e4ac56ed0e26fd6da34ad541acc2798f1aa
toulousain79 <toulousain79@users.noreply.github.com>
2017-12-01 12:36:53 +0100

    v5.1.1

--- templates/Dump_MySQL_MySB_db_Schema.sql
+++ templates/Dump_MySQL_MySB_db_Schema.sql
@@ -285,6 +285,7 @@ CREATE TABLE IF NOT EXISTS `system` (
   `rt_price_per_users` decimal(6,2) DEFAULT NULL DEFAULT '0.00',
   `rt_method` tinyint(1) NOT NULL DEFAULT '0',
   `proxy` tinyint(1) NOT NULL DEFAULT '0',
+  `ipv4_additional` varchar(128) NOT NULL,  
   PRIMARY KEY (`id_system`),
   UNIQUE KEY `mysb_version` (`mysb_version`,`mysb_user`,`mysb_password`,`hostname`,`ipv4`,`primary_inet`,`timezone`,`cert_password`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
