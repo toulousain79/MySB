0e0bbd83c5956f095eabb3759e35299bfcc17b9c
toulousain79 <1706911-toulousain79@users.noreply.gitlab.com>
2019-04-12 21:34:27 +0000

    Bugfix

--- web/pages/Layout.php
+++ web/pages/Layout.php
@@ -113,6 +113,7 @@ $MySB_Version = GetVersion();
 
 		<div id="site_content">
 			<div class="content">
+				<div class="scrollmenu" align="center">
 <?php
 			if ( isset($_SESSION['page']) ) {
 				switch ($_SESSION['page']) {
@@ -134,6 +135,7 @@ $MySB_Version = GetVersion();
 				if ($this->hasContent('extended')) echo $this->content('extended');
 			}
 ?>
+				</div>
 			</div>
 			<div id="sidebar_container">
 				<?php echo $this->content('sidebar', true); ?>
