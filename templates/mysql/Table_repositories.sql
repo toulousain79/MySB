d8096028f3973ba13977c741fb7daa313b2bd901
toulousain79 <toulousain79@users.noreply.github.com>
2019-07-03 23:21:26 +0200

    force update new version

--- templates/mysql/Table_repositories.sql
+++ templates/mysql/Table_repositories.sql
@@ -22,7 +22,7 @@
 LOCK TABLES `repositories` WRITE;
 /*!40000 ALTER TABLE `repositories` DISABLE KEYS */;
 INSERT INTO `repositories` (`type`, `dir`, `name`, `version`, `upgrade`, `file`, `url`, `active`, `on_boot`, `script`) VALUES
-('ZIP', '<InstallDir>/web/apps/ru', 'ruTorrent', '3.9_626ba3d', 1, 'ruTorrent_v3.9_626ba3d.zip', 'https://github.com/toulousain79/MySB_files/raw/v5.4/ruTorrent_v3.9_626ba3d.zip', 1, 0, ''),
+('ZIP', '<InstallDir>/web/apps/ru', 'ruTorrent', '3.9_626ba3d', 1, 'ruTorrent_v3.9_626ba3d.zip', 'https://github.com/toulousain79/MySB_files/raw/v6.0/ruTorrent_v3.9_626ba3d.zip', 1, 0, ''),
 ('TARGZ', '<InstallDir>/web/apps/ru/plugins/chat', 'ruTorrent Plugin Chat', '2.0', 0, 'chat_v2.0.tar.gz', 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/rutorrent-chat/chat-2.0.tar.gz', 1, 0, ''),
 ('TARGZ', '<InstallDir>/web/apps/ru/plugins/logoff', 'ruTorrent Plugin Logoff', '1.3', 0, 'logoff_v1.3.tar.gz', 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/rutorrent-logoff/logoff-1.3.tar.gz', 1, 0, ''),
 ('TARGZ', '<InstallDir>/web/apps/ru/plugins/lbll-suite', 'ruTorrent Plugin LBLL-Suite', '0.8.1', 0, 'lbll-suite_v0.8.1.tar.gz', 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/rutorrent-tadd-labels/lbll-suite_0.8.1.tar.gz', 1, 0, ''),
