64513b72c2f5caa28e58c9f0aa64a33c2052d66a
toulousain79 <toulousain79@users.noreply.github.com>
2019-10-25 22:33:28 +0200

    downloadall

--- templates/mysql/Table_repositories.sql
+++ templates/mysql/Table_repositories.sql
@@ -22,7 +22,7 @@
 LOCK TABLES `repositories` WRITE;
 /*!40000 ALTER TABLE `repositories` DISABLE KEYS */;
 INSERT INTO `repositories` (`type`, `dir`, `name`, `version`, `upgrade`, `file`, `url`, `active`, `on_boot`, `script`) VALUES
-('ZIP', '<InstallDir>/web/apps/ru', 'ruTorrent', '3.9_cdb4cfe', 1, 'ruTorrent_v3.9_cdb4cfe.zip', 'https://github.com/toulousain79/MySB_files/raw/v99.99/ruTorrent_v3.9_cdb4cfe.zip', 1, 0, ''),
+('ZIP', '<InstallDir>/web/apps/ru', 'ruTorrent', '3.9_cdb4cfe', 0, 'ruTorrent_v3.9_cdb4cfe.zip', 'https://github.com/toulousain79/MySB_files/raw/v99.99/ruTorrent_v3.9_cdb4cfe.zip', 1, 0, ''),
 ('TARGZ', '<InstallDir>/web/apps/ru/plugins/chat', 'ruTorrent Plugin Chat', '2.0', 0, 'chat_v2.0.tar.gz', 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/rutorrent-chat/chat-2.0.tar.gz', 1, 0, ''),
 ('TARGZ', '<InstallDir>/web/apps/ru/plugins/logoff', 'ruTorrent Plugin Logoff', '1.3', 0, 'logoff_v1.3.tar.gz', 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/rutorrent-logoff/logoff-1.3.tar.gz', 1, 0, ''),
 ('TARGZ', '<InstallDir>/web/apps/ru/plugins/lbll-suite', 'ruTorrent Plugin LBLL-Suite', '0.8.1', 0, 'lbll-suite_v0.8.1.tar.gz', 'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/rutorrent-tadd-labels/lbll-suite_0.8.1.tar.gz', 1, 0, ''),
@@ -47,7 +47,7 @@ INSERT INTO `repositories` (`type`, `dir`, `name`, `version`, `upgrade`, `file`,
 ('GIT', '<InstallDir>/web/apps/ru/plugins/theme/themes/MaterialDesign', 'ruTorrent Theme MaterialDesign', '0.1', 0, 'MaterialDesign_v0.1.zip', 'https://github.com/Phlooo/ruTorrent-MaterialDesign.git', 1, 0, ''),
 ('ZIP', '<InstallDir>/sources/libtorrent', 'LibTorrent', '0.13.8_3cec247', 0, 'libtorrent_v0.13.8_3cec247.zip', 'https://github.com/toulousain79/MySB_files/raw/v99.99/libtorrent_v0.13.8_3cec247.zip', 1, 0, ''),
 ('ZIP', '<InstallDir>/sources/rtorrent', 'rTorrent', '0.9.8_8ac98c6', 0, 'rtorrent_v0.9.8_8ac98c6.zip', 'https://github.com/toulousain79/MySB_files/raw/v99.99/rtorrent_v0.9.8_8ac98c6.zip', 1, 0, ''),
-('ZIP', '<InstallDir>/web/wolf', 'Wolf CMS', '0.8.3.1', 1, 'wolf_v0.8.3.1.zip', 'https://github.com/toulousain79/MySB_files/raw/v99.99/wolf_v0.8.3.1.zip', 1, 0, ''),
+('ZIP', '<InstallDir>/web/wolf', 'Wolf CMS', '0.8.3.1', 0, 'wolf_v0.8.3.1.zip', 'https://github.com/toulousain79/MySB_files/raw/v99.99/wolf_v0.8.3.1.zip', 1, 0, ''),
 ('CURL', '/usr/local/bin/docker-compose', 'Docker-Compose', 'latest', 0, 'docker-compose-Linux-x86_64', 'https://api.github.com/repos/docker/compose/releases/latest', 1, 0, ''),
 /* ('CURL', '/usr/local/bin/dry', 'Dry', 'latest', 0, 'dry-linux-amd64', 'https://api.github.com/repos/moncho/dry/releases/latest', 1, 0, ''), */
 ('CURL', '/usr/local/bin/dry', 'Dry', 'v0.10-beta.1', 0, 'dry-linux-amd64', 'https://github.com/moncho/dry/releases/download/v0.10-beta.1/dry-linux-amd64', 1, 0, ''),
