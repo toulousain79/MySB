2d11e40f1115d72fc1e1780982d2415a891a946f
toulousain79 <toulousain79@users.noreply.github.com>
2019-11-22 16:04:01 +0100

    ci python3 check fix

--- ci/scripts/70-check_python.sh
+++ ci/scripts/70-check_python.sh
@@ -37,18 +37,18 @@ if [ -n "${sFilesList}" ]; then
     done
 fi
 
-sFilesList="$(grep -IRl "\(#\!/usr/bin/env\) python3" --exclude-dir ".git" --exclude-dir ".vscode" --exclude-dir "ci" "${sDirToScan}/")"
-if [ -n "${sFilesList}" ]; then
-    echo && echo -e "${CBLUE}*** Check Python3 Syntax ***${CEND}"
-    for file in ${sFilesList}; do
-        if (! pylint3 "${file}"); then
-            echo -e "${CYELLOW}${file}:${CEND} ${CRED}Failed${CEND}"
-            nReturn=$((nReturn + 1))
-        else
-            echo -e "${CYELLOW}${file}:${CEND} ${CGREEN}Passed${CEND}"
-        fi
-    done
-fi
+# sFilesList="$(grep -IRl "\(#\!/usr/bin/env\) python3" --exclude-dir ".git" --exclude-dir ".vscode" --exclude-dir "ci" "${sDirToScan}/")"
+# if [ -n "${sFilesList}" ]; then
+#     echo && echo -e "${CBLUE}*** Check Python3 Syntax ***${CEND}"
+#     for file in ${sFilesList}; do
+#         if (! pylint3 "${file}"); then
+#             echo -e "${CYELLOW}${file}:${CEND} ${CRED}Failed${CEND}"
+#             nReturn=$((nReturn + 1))
+#         else
+#             echo -e "${CYELLOW}${file}:${CEND} ${CGREEN}Passed${CEND}"
+#         fi
+#     done
+# fi
 
 export nReturn
 
