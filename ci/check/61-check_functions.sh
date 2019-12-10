8cf78fa19a1ad5d6ae5045f7aa1e2f7c0827112c
toulousain79 <toulousain79@users.noreply.github.com>
2019-12-10 12:00:32 +0100

    test with /bin/true

--- ci/check/61-check_functions.sh
+++ ci/check/61-check_functions.sh
@@ -60,23 +60,38 @@ if [ -n "${sFilesList}" ]; then
             # ((nCount++))
             # sService="${sColumns[${nCount}]//.service/}"
 
-            # if (grep -q 'daemon-reload' <<<"${sROW}"); then
-            #     # echo "${sFile}: systemctl daemon-reload --> #systemctl daemon-reload"
-            #     (! sed -i -e "s/systemctl daemon-reload/#systemctl daemon-reload/g" "${sFile}") && ((nRes++))
-            # elif (grep -q 'systemctl reboot' <<<"${sROW}"); then
-            #     # echo "${sFile}: systemctl reboot --> #systemctl reboot"
-            #     (! sed -i -e "s/systemctl reboot/#systemctl reboot/g" "${sFile}") && ((nRes++))
-            # elif (grep -q ' disable' <<<"${sROW}"); then
-            #     # echo "${sFile}: systemctl disable ${sService} --> update-rc.d ${sService} disable"
-            #     (! sed -i -e "s/systemctl disable ${sService}/update-rc.d ${sService} disable/g" "${sFile}") && ((nRes++))
-            # elif (grep -q ' enable' <<<"${sROW}"); then
-            #     # echo "${sFile}: systemctl enable ${sService} --> update-rc.d ${sService} enable"
-            #     (! sed -i -e "s/systemctl enable ${sService}/update-rc.d ${sService} enable/g" "${sFile}") && ((nRes++))
-            # else
-            #     # echo "${sFile}: systemctl ${sSwitch} ${sService} --> service ${sService} ${sSwitch}"
-            #     (! sed -i -e "s/systemctl ${sSwitch} ${sService}/service ${sService} ${sSwitch}/g" "${sFile}") && ((nRes++))
-            # fi
-            echo "${sROW}"
+            if (grep -q 'daemon-reload' <<<"${sROW}"); then
+                # echo "${sFile}: systemctl daemon-reload --> #systemctl daemon-reload"
+                (! sed -i -e "s/systemctl daemon-reload/#systemctl daemon-reload/g" "${sFile}") && {
+                    /bin/true
+                    ((nRes++))
+                }
+            elif (grep -q 'systemctl reboot' <<<"${sROW}"); then
+                # echo "${sFile}: systemctl reboot --> #systemctl reboot"
+                (! sed -i -e "s/systemctl reboot/#systemctl reboot/g" "${sFile}") && {
+                    /bin/true
+                    ((nRes++))
+                }
+            elif (grep -q ' disable' <<<"${sROW}"); then
+                # echo "${sFile}: systemctl disable ${sService} --> update-rc.d ${sService} disable"
+                (! sed -i -e "s/systemctl disable ${sService}/update-rc.d ${sService} disable/g" "${sFile}") && {
+                    /bin/true
+                    ((nRes++))
+                }
+            elif (grep -q ' enable' <<<"${sROW}"); then
+                # echo "${sFile}: systemctl enable ${sService} --> update-rc.d ${sService} enable"
+                (! sed -i -e "s/systemctl enable ${sService}/update-rc.d ${sService} enable/g" "${sFile}") && {
+                    /bin/true
+                    ((nRes++))
+                }
+            else
+                # echo "${sFile}: systemctl ${sSwitch} ${sService} --> service ${sService} ${sSwitch}"
+                (! sed -i -e "s/systemctl ${sSwitch} ${sService}/service ${sService} ${sSwitch}/g" "${sFile}") && {
+                    /bin/true
+                    ((nRes++))
+                }
+            fi
+            echo "${sFile}: ${sROW}"
         done < <(grep 'systemctl ' "${sFile}")
 
         # shellcheck disable=SC2181
