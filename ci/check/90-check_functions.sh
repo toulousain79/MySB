86381db6c1ac7d1854cf5f9c894853720419bc6b
toulousain79 <toulousain79@users.noreply.github.com>
2019-12-13 10:17:17 +0100

    test

--- ci/check/90-check_functions.sh
+++ ci/check/90-check_functions.sh
@@ -145,14 +145,14 @@ case "${CHECK_METHOD}" in
                 for sFile in ${sFilesList}; do
                     echo "${sFile}"
                     # shellcheck source=/dev/null
-                    cat "${sFile}"
+                    source "${sFile}"
 
                     sIntegFile="${MySB_InstallDir}/ci/integ/$(basename "${sFile}").sh"
                     sIntegFile="${sIntegFile//.bsh/}"
                     echo "l.155 ${sIntegFile}"
                     if [ -f "${sIntegFile}" ]; then
                         echo "l.157 ${sIntegFile}"
-                        cat "${sIntegFile}"
+                        source "${sIntegFile}"
                     fi
                     echo
                 done
